ff663ea00a0508d3b471cf443dd30d16e975d93d
PolliBu <8polli.bu@gmail.com>
2024-02-25 14:03:19 +0200

    phone

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import css from './Contact.module.css';
 import { useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/operations';
 
-const Contact = ({ contact: { name, number, id } }) => {
+const Contact = ({ contact: { name, phone, id } }) => {
   const dispatch = useDispatch();
   const handleDelete = () => dispatch(deleteContact(id));
 
@@ -15,7 +15,7 @@ const Contact = ({ contact: { name, number, id } }) => {
           <FaUser /> {name}
         </p>
         <p>
-          <FaPhone /> {number}
+          <FaPhone /> {phone}
         </p>
       </div>
       <button className={css.btn} onClick={handleDelete}>
