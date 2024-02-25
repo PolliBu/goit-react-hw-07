67a9932ec6d9d42f1048e390e8d28d953c5b664b
PolliBu <8polli.bu@gmail.com>
2024-02-25 12:22:50 +0200

    selectVisibleContacts

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,24 @@
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 import { useSelector } from 'react-redux';
-import { selectContacts, selectContactsFilter } from '../../redux/selectors';
+// import { selectContacts, selectContactsFilter } from '../../redux/selectors';
+import { selectVisibleContacts } from '../../redux/selectors';
 
-const selectFilteredContacts = (contacts, filter) => {
-  const normalizedFilter = filter.toLowerCase();
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(normalizedFilter),
-  );
-};
+// const selectFilteredContacts = (contacts, filter) => {
+//   const normalizedFilter = filter.toLowerCase();
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  const filter = useSelector(selectContactsFilter);
-  const filteredContacts = selectFilteredContacts(contacts, filter);
+  const contacts = useSelector(selectVisibleContacts);
+  // const filter = useSelector(selectContactsFilter);
+  // const filteredContacts = selectVisibleContacts(contacts, filter);
 
   return (
     <ul className={css.list}>
-      {filteredContacts.map(contact => (
+      {contacts.map(contact => (
         <li key={contact.id}>
           <Contact contact={contact} />
         </li>
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectContacts = state => state.contacts.items;
 
 export const selectContactsFilter = state => state.filters.name;
@@ -5,3 +7,18 @@ export const selectContactsFilter = state => state.filters.name;
 export const selectLoading = state => state.contacts.isLoading;
 
 export const selectError = state => state.contacts.error;
+
+export const selectVisibleContacts = createSelector(
+  [selectContacts, selectContactsFilter],
+  (contacts, filter) => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  },
+);
+// const selectFilteredContacts = (contacts, filter) => {
+//   const normalizedFilter = filter.toLowerCase();
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(normalizedFilter),
+//   );
+// };
